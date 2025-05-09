88ca47d0624389117f003c09a598769ccb3ddcbf
evgeniya.shumakova <evgeniya.shumakova@ic-group.ru>
2025-05-09 13:08:42 +0300

    ✨ feat(hook): Add root path and link options to hook configuration 📝 docs(readme): Add pre-commit hook usage example ♻️ refactor(main): Simplify CLI arguments and output handling

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ## :fairy::sparkles: Cute tool for building content trees
 
+Pre-commit hook example
 
+```yaml
+-   repo: https://github.com/M-Astrid/path-pixie
+    rev: 0.1.0
+    hooks:
+      - id: path-pixie
+        name: path-pixie
+        args: [--output, readme.md, --depth, "5"]
+```
--- path_pixie/hook.py
+++ path_pixie/hook.py
@@ -1,32 +1,26 @@
 import argparse
-from typing import Any
 
 from path_pixie.common.const import DEFAULT_DEPTH
-from path_pixie.renderer.adapter.renderer import MDListRenderer, TextRenderer
+from path_pixie.renderer.adapter.renderer import MDListRenderer
 from path_pixie.saver.adapter.file import FileOutputSaver
 from path_pixie.tree.adapter.composite import Project
 
 
-def run(**kwargs: Any) -> None:
+def run() -> None:
     parser = argparse.ArgumentParser(description="Cute directory tree generator.")
 
-    parser.add_argument(
-        "--format", type=str, default="md", choices=["md", "txt"], help="Формат вывода: текст или markdown."
-    )
     parser.add_argument("--depth", type=int, default=DEFAULT_DEPTH, help="Максимальная глубина обхода.")
-    parser.add_argument("--output", type=str, default=None, help="Имя файла для вывода результата.")
+    parser.add_argument("--output", type=str, default="readme.md", help="Имя файла для вывода результата.")
+    parser.add_argument("--root", type=str, default=".", help="Путь к корневой директории.")
+    parser.add_argument("--is_links", type=bool, default=True, help="Ссылки вместо обычных текстовых имен.")
 
     args = parser.parse_known_args()[0]
 
-    depth, format_ = args.depth, args.format
+    depth, output, root = args.depth, args.output, args.root
 
-    render = MDListRenderer() if format_ == "md" else TextRenderer()
-    project = Project("knowledge_base")
+    render = MDListRenderer()
+    project = Project(root)
     tree = project.get_content(max_depth=depth)
 
-    formatted = render(tree, is_links=True, depth=depth)
-
-    if args.output:
-        FileOutputSaver("readme.md")(formatted)
-    else:
-        print(formatted)
+    formatted = render(tree, is_links=args.is_links, depth=depth)
+    FileOutputSaver(output)(formatted)
